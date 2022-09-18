c911847b6d7600c3b1e563a06276d7767ca3ef79
alex.s <shikorin@mail.ru>
2022-09-18 22:02:49 +0300

    add slick slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,45 @@
 import slider from './modules/slider';
+import $ from 'jquery';
+import 'slick-carousel';
+
 
 window.addEventListener('DOMContentLoaded', function () {
 
+    $('.partners__list').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
 
     slider({
         container: '.promo__slider',
@@ -14,16 +52,16 @@ window.addEventListener('DOMContentLoaded', function () {
         padLeftRight: 0
     });
 
-    slider({
-        container: '.partners__content',
-        slide: '.partners__item',
-        nextArrow: '.partners .promo__slider-next',
-        prevArrow: '.partners .promo__slider-prev',
-        wrapper: '.partners__wrapper',
-        field: '.partners__list',
-        widthSlide: 5,
-        padLeftRight: 17.5
-    });
+    // slider({
+    //     container: '.partners__content',
+    //     slide: '.partners__item',
+    //     nextArrow: '.partners .promo__slider-next',
+    //     prevArrow: '.partners .promo__slider-prev',
+    //     wrapper: '.partners__wrapper',
+    //     field: '.partners__list',
+    //     widthSlide: 5,
+    //     padLeftRight: 17.5
+    // });
     
 
 
