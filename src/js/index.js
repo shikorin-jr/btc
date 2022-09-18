b5cc2621d2dd970f80825fa55e2fe92f2c64e585
alex.s <shikorin@mail.ru>
2022-09-18 23:15:09 +0300

    insert style slick-theme

--- src/js/index.js
+++ src/js/index.js
@@ -20,24 +20,8 @@ window.addEventListener('DOMContentLoaded', function () {
               infinite: true,
               dots: true
             }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
           }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
+
         ]
       });
 
